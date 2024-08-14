a4f7c3b33d92857d70629223e3926d203d91fea8
nathan-zhy <139937160+nathan-zhy@users.noreply.github.com>
2024-08-14 19:47:14 -0400

    v0.1

---
+++ src/components/WUSTClassSearch.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+const value = ref('');
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <van-field v-model="value" label="Search Course" placeholder="Please insert course number" />
+</template>
