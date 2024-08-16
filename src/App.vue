98eade76cae068a0015875f0f60bf69a425d6c7d
nathan-zhy <139937160+nathan-zhy@users.noreply.github.com>
2024-08-16 16:39:02 -0400

    update list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import ScheduleOnline from './components/ScheduleOnline.vue';
-import { windowHeight, windowWidth } from 'vant/lib/utils';
-//import 'amfe-flexible'
 
 
 const IsMobile = ref(null == navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))
--- src/components/ScheduleOnline.vue
+++ src/components/ScheduleOnline.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { getCurrentInstance, onMounted, ref } from "vue";
 import { active, arr, value, list, value1, value2, option1, option2 } from "./ScheduleOnline"
 import { showToast } from "vant";
 import { windowHeight, windowWidth } from "vant/lib/utils";
@@ -14,22 +14,23 @@ const onRefresh = () => {
 	}, 1000);
 };
 
-const listCon = {
-	padding: 1,
-	width: 100,
-	height: 50,
-	top: 10,
-}
+const navbar = ref()
+const tabbar = ref()
+
+onMounted(() => {
+	console.log(navbar.value.height)
+})
 </script>
 
 <template>
-	<div :style="{
+	<div ref="navbar" :style="{
 		padding: '5px 5px', backgroundColor: 'yellow'
 	}">
 		<van-nav-bar title="标题" fixed />
 	</div>
 	<div :style="{
-		padding: '5px 5px', backgroundColor: 'red'
+		padding: '5px 5px', backgroundColor: 'red', width: windowWidth + 'px',
+		height: windowHeight + 'px'
 	}">
 		<van-dropdown-menu swipe-threshold="4">
 			<van-dropdown-item v-model="value1" :options="option1" />
@@ -43,7 +44,7 @@ const listCon = {
 		</van-pull-refresh>
 	</div>
 
-	<div :style="{
+	<div ref="tabbar" :style="{
 		padding: '5px 5px', backgroundColor: 'blue'
 	}">
 		<van-tabbar>
